40f2108f0dec73b3e2a6d4b3a8cf6b5f02b5bd05
thrwwy216 <thrwwy216@tutanota.com>
2021-01-14 17:00:23 -0600

    init commit

---
+++ src/main/java/com/kwgdev/microbeerorderservice/MicroBeerOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kwgdev.microbeerorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBeerOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroBeerOrderServiceApplication.class, args);
+    }
+
+}
